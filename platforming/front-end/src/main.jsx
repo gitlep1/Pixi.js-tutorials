ce2b25fc4035cc55fa0d38707ea096a69dbc5616
gitlep1 <55455906+gitlep1@users.noreply.github.com>
2024-11-22 16:54:53 -0500

    Updated: app.jsx and main.jsx initial code removed and their respective styling files switched to scss instead of the default css

--- platforming/front-end/src/main.jsx
+++ platforming/front-end/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import "react-loading-skeleton/dist/skeleton.css";
+import "./main.scss";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
